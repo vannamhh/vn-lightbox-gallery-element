f81f5e27134c83552b61fb2a90118aabc4a29cb9
Hoang Nam <vannamhh@gmail.com>
2025-11-18 20:37:52 +0700

    update select page

--- includes/class-vn-ux-builder.php
+++ includes/class-vn-ux-builder.php
@@ -67,10 +67,11 @@ class VN_UX_Builder {
 				'template' => $this->get_shortcode_template(),
 				'options'  => array(
 					'post_id' => array(
-						'type'        => 'textfield',
-						'heading'     => __( 'Post ID (Tùy chọn)', 'vn-lightbox-gallery' ),
-						'description' => __( 'Bỏ trống để lấy trang hiện tại', 'vn-lightbox-gallery' ),
+						'type'        => 'select',
+						'heading'     => __( 'Chọn trang Gallery', 'vn-lightbox-gallery' ),
+						'description' => __( 'Chọn trang có dữ liệu gallery', 'vn-lightbox-gallery' ),
 						'default'     => '',
+						'options'     => $this->get_pages_list(),
 					),
 					'filters' => array(
 						'type'    => 'checkbox',
@@ -101,6 +102,38 @@ class VN_UX_Builder {
 	 * @return string Shortcode template.
 	 */
 	private function get_shortcode_template(): string {
-		return '[vn_gallery post_id="{{post_id}}" filters="{{filters}}" show_title="{{show_title}}" class="{{class}}"]';
+		return '[vn_gallery{{post_id ? \' post_id="\' + post_id + \'"\' : \'\'}}{{filters ? \' filters="\' + filters + \'"\' : \'\'}}{{show_title ? \' show_title="\' + show_title + \'"\' : \'\'}}{{class ? \' class="\' + class + \'"\' : \'\'}}]';
+	}
+
+	/**
+	 * Get list of pages for dropdown.
+	 *
+	 * @return array Pages list with ID => Title format.
+	 */
+	private function get_pages_list(): array {
+		$pages = array(
+			'' => __( '-- Trang hiện tại --', 'vn-lightbox-gallery' ),
+		);
+
+		// Get all pages.
+		$query = new WP_Query(
+			array(
+				'post_type'      => 'page',
+				'posts_per_page' => -1,
+				'orderby'        => 'title',
+				'order'          => 'ASC',
+				'post_status'    => 'publish',
+			)
+		);
+
+		if ( $query->have_posts() ) {
+			while ( $query->have_posts() ) {
+				$query->the_post();
+				$pages[ get_the_ID() ] = get_the_title() . ' (ID: ' . get_the_ID() . ')';
+			}
+			wp_reset_postdata();
+		}
+
+		return $pages;
 	}
 }
