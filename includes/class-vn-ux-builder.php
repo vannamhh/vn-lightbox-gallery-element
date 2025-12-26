ac77fc54deed9df29dbf98da675e87cfa5a39ac7
Hoang Nam <vannamhh@gmail.com>
2025-12-26 23:15:39 +0700

    feat: Refactor VN UX Builder to streamline gallery selection and improve performance

--- includes/class-vn-ux-builder.php
+++ includes/class-vn-ux-builder.php
@@ -66,50 +66,24 @@ class VN_UX_Builder {
 				'icon'     => 'text',
 				'template' => $this->get_shortcode_template(),
 				'options'  => array(
-					'source_type'        => array(
-						'type'    => 'select',
-						'heading' => __( 'Nguồn Gallery', 'vn-lightbox-gallery' ),
-						'default' => 'page',
-						'options' => array(
-							'page'        => __( 'Page', 'vn-lightbox-gallery' ),
-							'custom_post' => __( 'Custom Post Type', 'vn-lightbox-gallery' ),
-						),
-					),
-					'custom_post_type'   => array(
+					'gallery_id' => array(
 						'type'        => 'select',
-						'heading'     => __( 'Loại Custom Post', 'vn-lightbox-gallery' ),
-						'description' => __( 'Chọn loại custom post type', 'vn-lightbox-gallery' ),
+						'heading'     => __( 'Chọn Gallery', 'vn-lightbox-gallery' ),
+						'description' => __( 'Chọn gallery bạn muốn hiển thị', 'vn-lightbox-gallery' ),
 						'default'     => '',
-						'options'     => $this->get_custom_post_types(),
-						'conditions'  => 'source_type === "custom_post"',
+						'options'     => $this->get_gallery_list(),
 					),
-					'page_id'            => array(
-						'type'        => 'select',
-						'heading'     => __( 'Chọn trang Gallery', 'vn-lightbox-gallery' ),
-						'description' => __( 'Chọn trang có dữ liệu gallery', 'vn-lightbox-gallery' ),
-						'default'     => '',
-						'options'     => $this->get_pages_list(),
-						'conditions'  => 'source_type === "page"',
-					),
-					'custom_post_id'     => array(
-						'type'        => 'select',
-						'heading'     => __( 'Chọn Custom Post', 'vn-lightbox-gallery' ),
-						'description' => __( 'Chọn custom post có dữ liệu gallery', 'vn-lightbox-gallery' ),
-						'default'     => '',
-						'options'     => $this->get_custom_posts_list(),
-						'conditions'  => 'source_type === "custom_post"',
-					),
-					'filters'            => array(
+					'filters'    => array(
 						'type'    => 'checkbox',
 						'heading' => __( 'Hiển thị Nút Lọc', 'vn-lightbox-gallery' ),
 						'default' => 'true',
 					),
-					'show_title'         => array(
+					'show_title' => array(
 						'type'    => 'checkbox',
 						'heading' => __( 'Hiển thị Tiêu đề', 'vn-lightbox-gallery' ),
 						'default' => '',
 					),
-					'class'              => array(
+					'class'      => array(
 						'type'        => 'textfield',
 						'heading'     => __( 'Class', 'vn-lightbox-gallery' ),
 						'description' => __( 'Thêm custom CSS class', 'vn-lightbox-gallery' ),
@@ -128,116 +102,43 @@ class VN_UX_Builder {
 	 * @return string Shortcode template.
 	 */
 	private function get_shortcode_template(): string {
-		return '[vn_gallery{{source_type ? \' source_type="\' + source_type + \'"\' : \'\'}}{{page_id ? \' page_id="\' + page_id + \'"\' : \'\'}}{{custom_post_id ? \' custom_post_id="\' + custom_post_id + \'"\' : \'\'}}{{custom_post_type ? \' custom_post_type="\' + custom_post_type + \'"\' : \'\'}}{{filters ? \' filters="\' + filters + \'"\' : \'\'}}{{show_title ? \' show_title="\' + show_title + \'"\' : \'\'}}{{class ? \' class="\' + class + \'"\' : \'\'}}]';
+		return '[vn_gallery{{gallery_id ? \' gallery_id="\' + gallery_id + \'"\' : \'\'}}{{filters ? \' filters="\' + filters + \'"\' : \'\'}}{{show_title ? \' show_title="\' + show_title + \'"\' : \'\'}}{{class ? \' class="\' + class + \'"\' : \'\'}}]';
 	}
 
 	/**
-	 * Get list of pages for dropdown.
+	 * Get list of galleries for dropdown.
 	 *
-	 * @return array Pages list with ID => Title format.
+	 * Fixed post_type='gallery' for optimal performance and UX.
+	 *
+	 * @return array Galleries list with ID => Title format.
 	 */
-	private function get_pages_list(): array {
-		$pages = array(
-			'' => __( '-- Trang hiện tại --', 'vn-lightbox-gallery' ),
+	private function get_gallery_list(): array {
+		$galleries = array(
+			'' => __( '-- Gallery hiện tại --', 'vn-lightbox-gallery' ),
 		);
 
-		// Get all pages.
+		// Query directly from 'gallery' post type - optimized & clean.
 		$query = new WP_Query(
 			array(
-				'post_type'      => 'page',
-				'posts_per_page' => -1,
-				'orderby'        => 'title',
-				'order'          => 'ASC',
-				'post_status'    => 'publish',
+				'post_type'              => 'gallery',
+				'posts_per_page'         => 100,
+				'orderby'                => 'title',
+				'order'                  => 'ASC',
+				'post_status'            => 'publish',
+				'no_found_rows'          => true,
+				'update_post_meta_cache' => false,
+				'update_post_term_cache' => false,
 			)
 		);
 
 		if ( $query->have_posts() ) {
 			while ( $query->have_posts() ) {
 				$query->the_post();
-				$pages[ get_the_ID() ] = get_the_title() . ' (ID: ' . get_the_ID() . ')';
+				$galleries[ get_the_ID() ] = get_the_title() . ' (ID: ' . get_the_ID() . ')';
 			}
 			wp_reset_postdata();
 		}
 
-		return $pages;
-	}
-
-	/**
-	 * Get list of available custom post types.
-	 *
-	 * @return array Custom post types list.
-	 */
-	private function get_custom_post_types(): array {
-		$post_types = array(
-			'' => __( '-- Chọn loại custom post --', 'vn-lightbox-gallery' ),
-		);
-
-		// Get all registered custom post types.
-		$args = array(
-			'public'   => true,
-			'_builtin' => false,
-		);
-
-		$registered_post_types = get_post_types( $args, 'objects' );
-
-		if ( ! empty( $registered_post_types ) ) {
-			foreach ( $registered_post_types as $post_type ) {
-				$post_types[ $post_type->name ] = $post_type->label;
-			}
-		}
-
-		return $post_types;
-	}
-
-	/**
-	 * Get list of custom posts for dropdown.
-	 *
-	 * This function is called when rendering the options,
-	 * so it returns all custom posts from all types.
-	 * The actual filtering by custom_post_type happens in JavaScript
-	 * via the conditions parameter in UX Builder.
-	 *
-	 * @return array Custom posts list with ID => Title format.
-	 */
-	private function get_custom_posts_list(): array {
-		$posts = array(
-			'' => __( '-- Chọn custom post --', 'vn-lightbox-gallery' ),
-		);
-
-		// Get all custom post types.
-		$args = array(
-			'public'   => true,
-			'_builtin' => false,
-		);
-
-		$post_types = get_post_types( $args );
-
-		if ( empty( $post_types ) ) {
-			return $posts;
-		}
-
-		// Get posts from all custom post types.
-		$query = new WP_Query(
-			array(
-				'post_type'      => array_values( $post_types ),
-				'posts_per_page' => -1,
-				'orderby'        => 'title',
-				'order'          => 'ASC',
-				'post_status'    => 'publish',
-			)
-		);
-
-		if ( $query->have_posts() ) {
-			while ( $query->have_posts() ) {
-				$query->the_post();
-				$post_type_obj   = get_post_type_object( get_post_type() );
-				$post_type_label = $post_type_obj ? $post_type_obj->labels->singular_name : get_post_type();
-				$posts[ get_the_ID() ] = get_the_title() . ' (' . $post_type_label . ' - ID: ' . get_the_ID() . ')';
-			}
-			wp_reset_postdata();
-		}
-
-		return $posts;
+		return $galleries;
 	}
 }
